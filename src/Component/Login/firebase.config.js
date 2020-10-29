b03cc0b5281b6ab15634825ba20cb86096012761
ayshi roy <ayshiroy22@gmail.com>
2020-10-28 17:42:10 -0700

    finish

---
+++ src/Component/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCVL0GOJ_h3LrfAxyw-WK6HSdNe6Y0AmSM",
+    authDomain: "creative-agency-app-7d54b.firebaseapp.com",
+    databaseURL: "https://creative-agency-app-7d54b.firebaseio.com",
+    projectId: "creative-agency-app-7d54b",
+    storageBucket: "creative-agency-app-7d54b.appspot.com",
+    messagingSenderId: "189747729634",
+    appId: "1:189747729634:web:1061ad19bfa479ee527abb"
+  };
+
+export default firebaseConfig;  
